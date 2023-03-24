03772fc1b325200544c72a11c54d263af3ae10d9
Mwayanjana Bolokonya <mwayab@gmail.com>
2023-03-24 08:34:27 +0200

    added landing page

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -8,19 +8,21 @@ import './App.css';
 import Home from './components/Home';
 import CashPoints from './components/CashPoints';
 import Dao from './components/Dao';
+import LandingPage from './components/LandingPage';
 
 
 function App() {
   return ( <>
     <BrowserRouter>
       <Routes>
+        <Route path='/Landing' element={<LandingPage/>}></Route>
         <Route path='/Home' element={<Home />}>
         </Route>
         <Route path='/CashPoints' element={<CashPoints />}>
         </Route>
         <Route path='/Dao' element={<Dao />}>
         </Route>
-          <Route path='/'  element={<Home />}>
+          <Route path='/'  element={<LandingPage />}>
           </Route>
           </Routes>
     </BrowserRouter> 
