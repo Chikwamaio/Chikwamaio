531019b6cbefc67d063f56c95a2e1c02ab9a156f
Mwayanjana Bolokonya <mwayab@gmail.com>
2023-11-16 14:52:36 +0200

    added how it works video to landing page

--- client/src/components/LandingPage.jsx
+++ client/src/components/LandingPage.jsx
@@ -2,26 +2,41 @@ import { useEffect } from 'react';
 import { useNavigate } from "react-router-dom";
 
 const LandingPage = () => {
-
     const { ethereum } = window;
     const navigate = useNavigate();
 
     useEffect(() => {
         ethereum ? navigate('/Home') : null;
-
     }, []);
 
     return (
         <div>
             <h2 className='md:text-3xl text-3xl text-slate-700 lg:text-6xl uppercase'> Welcome to</h2>
             <h1 className='text-3xl md:text-3xl text-slate-700 lg:text-8xl font-bold uppercase mb-8'>Chikwama</h1>
+
+            {/* Embedded YouTube video */}
+            <div className="py-4">
+                <p className='py-4 text-slate-700'>How it Works:</p>
+                <iframe
+                    width="560"
+                    height="315"
+                    src="https://www.youtube.com/embed/P__KTNLBWLc?si=5bEb7hAI99uBfj1X"
+                    title="YouTube video player"
+                    frameBorder="0"
+                    allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+                    allowFullScreen
+                ></iframe>
+            </div>
+
             <p className='py-4 text-slate-700'>Why use Chikwama?</p>
             <ul className='list-disc pl-8'>
                 <li className='text-slate-700'>Find Cashpoints: Locate nearby cashpoints where you can convert xDai to local currency effortlessly.</li>
                 <li className='text-slate-700'>Nominal Fee: Chikwama charges a nominal fee of 1% for providing the cashpoint conversion service.</li>
             </ul>
-            <p className='py-4 text-slate-700'>To get started, install <a className='text-fuchsia-700' href='https://metamask.io/'>MetaMask</a> (or another Ethereum-compatible wallet) and experience the future of decentralized finance with Chikwama.</p>
-            <p className='py-4 text-slate-700'>If you have any questions or need help getting started with Chikwama or MetaMask, please don't hesitate to reach out to us.</p>
+
+            <p className='py-4 text-slate-700'>
+                If you have any questions or need help getting started with Chikwama or MetaMask, please don't hesitate to <a href="mailto:info@chikwama.net" className='text-fuchsia-700'>reach out to us</a>.
+            </p>
         </div>
     );
 }
