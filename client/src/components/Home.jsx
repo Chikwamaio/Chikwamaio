5e5f8f98525db0135fcba58d1ccaa13c0eb6d6ee
Mwayanjana Bolokonya <mwayab@gmail.com>
2025-03-19 16:30:34 +0200

    removed unnecessary imports

--- client/src/components/Home.jsx
+++ client/src/components/Home.jsx
@@ -3,15 +3,12 @@ import CalculateIcon from '@mui/icons-material/Calculate';
 import PieChartIcon from '@mui/icons-material/PieChart';
 import Fade from '@mui/material/Fade';
 import HelpOutlineIcon from '@mui/icons-material/HelpOutline';
-import Snackbar from '@mui/material/Snackbar';
 import { ethers } from 'ethers';
 import { useEffect, useState } from 'react';
 import { useNavigate } from "react-router-dom";
 import cashPoints from '../../../contracts/artifacts/contracts/Cashpoints.sol/CashPoints.json';
 import Footer from './Footer';
 import NavBar from './NavBar';
-import SendMoney from './SendMoney';
-import { use } from 'chai';
 import { renderMetaMaskPrompt } from './InstallMetaMask';
 
 
